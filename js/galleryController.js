330455e77a9ddfdd2a9fce238ef658737f04ac31
MaorKikoz <maorkikoz2@gmail.com>
2026-06-13 19:15:54 +0300

    gallery is finally rendered

--- js/galleryController.js
+++ js/galleryController.js
@@ -1,11 +1,11 @@
 'use strict'
 
 function renderGallery(gallery) {
-   	const elGallery = document.querySelector('gallery')
+   	const elGallery = document.querySelector('.gallery')
 	const imgs = gallery
 
 	var galleryHtml = ''
-	for (var i = 0; i <= imgs.length; i++) {
+	for (var i = 0; i < imgs.length; i++) {
 		//const txt = getTxt(txtLength)
         var imgUrl = imgs[i].url
                 console.log(imgs[i])
